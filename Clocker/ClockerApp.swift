27e1740d2314b478aa6bc4f67f4f8a5285a09490
Radosław Dąbrowski <radoslaw.dab.dabrowski@gmail.com>
2023-12-15 13:59:31 +0100

    Initial Commit

---
+++ Clocker/ClockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockerApp.swift
+//  Clocker
+//
+//  Created by Radosław Dąbrowski on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
