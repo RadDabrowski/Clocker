b5e2796c27b5705febb97a9d609711fc6f9e6d3a
Radosław Dąbrowski <radoslaw.dab.dabrowski@gmail.com>
2024-01-14 11:59:11 +0100

    adding Entry model.

---
+++ Clocker/Models/Entry.swift
@@ -0,0 +1,21 @@
+//
+//  Entry.swift
+//  Clocker
+//
+//  Created by Radosław Dąbrowski on 14/01/2024.
+//
+
+import Foundation
+
+struct Entry {
+    var startTime: Date
+    var endTime: Date
+    var breakDurationMinutes: Int
+    var hourlyRate: Double
+    var bonus: Double
+    var earnings: Double
+    var client: String
+    var project: String
+    var task: String
+    var notes: String
+}
--- Clocker/Views/MoreView.swift
+++ Clocker/Views/MoreView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
 struct MoreView_Previews: PreviewProvider {
     static var previews: some View {
         MoreView()
